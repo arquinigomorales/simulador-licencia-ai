575a7ce50471f9b16dabc4cfe1f42fd92e36de07
Carlos Arquinigo <arquinigomorales@gmail.com>
2026-08-20 14:36:40 -0500

    Agregar lógica del simulador

---
+++ js/preguntas.js
@@ -0,0 +1,143 @@
+const bancoPreguntas = [
+
+    {
+        id: 1,
+        categoria: "Reglamento de Tránsito",
+        question: "Está permitido en la vía:",
+        answers: [
+            "Recoger o dejar pasajeros o carga en cualquier lugar.",
+            "Dejar animales sueltos o situarlos de forma tal que obstaculicen solo un poco el tránsito.",
+            "Recoger o dejar pasajeros en lugares autorizados.",
+            "Ejercer el comercio ambulatorio o estacionario."
+        ],
+        correct: 2,
+        image: null
+    },
+
+    {
+        id: 2,
+        categoria: "Reglamento de Tránsito",
+        question: "Respecto de los dispositivos de control o regulación del tránsito:",
+        answers: [
+            "Solo los peatones están obligados a su obediencia.",
+            "Los conductores y los peatones están obligados a su obediencia, salvo instrucción de la Policía Nacional del Perú asignada al tránsito que indique lo contrario.",
+            "Solo los conductores están obligados a su obediencia.",
+            "Los conductores están obligados a su obediencia, aun cuando la Policía Nacional del Perú asignada al tránsito pueda indicar lo contrario."
+        ],
+        correct: 1,
+        image: null
+    },
+
+    {
+        id: 5,
+        categoria: "Señalización",
+        question: "En las vías, las marcas en el pavimento que son del tipo central discontinua y de color amarillo significan que:",
+        answers: [
+            "Está permitido cruzar al otro carril para el adelantamiento vehicular, si es que es seguro hacerlo.",
+            "No está permitido cruzar al otro carril para el adelantamiento vehicular.",
+            "Se está reduciendo el ancho de la calzada de la vía por donde se circula.",
+            "Se está frente a un lugar de cruce peatonal."
+        ],
+        correct: 0,
+        image: null
+    },
+
+    {
+        id: 6,
+        categoria: "Semáforos",
+        question: "El color ámbar o amarillo del semáforo significa que:",
+        answers: [
+            "Los vehículos deben avanzar.",
+            "Los vehículos deben detenerse.",
+            "Los vehículos deben acelerar la marcha.",
+            "Los vehículos deben detenerse antes de ingresar a la intersección si su velocidad y ubicación lo permiten; de lo contrario, deberán cruzar y despejar la intersección."
+        ],
+        correct: 3,
+        image: null
+    },
+
+    {
+        id: 12,
+        categoria: "Semáforos",
+        question: "¿La luz intermitente roja es igual que una señal de PARE?",
+        answers: [
+            "Verdad.",
+            "Es verdad siempre y cuando también se encuentre un Policía de Tránsito indicando la señal de Pare.",
+            "Falso.",
+            "Ninguna de las alternativas es correcta."
+        ],
+        correct: 0,
+        image: null
+    },
+
+    {
+        id: 17,
+        categoria: "Reglamento de Tránsito",
+        question: "Está prohibido estacionar un vehículo:",
+        answers: [
+            "En las curvas.",
+            "Dentro de una intersección.",
+            "Frente a la entrada de garajes y de recintos militares o policiales.",
+            "Todas las alternativas son correctas."
+        ],
+        correct: 3,
+        image: null
+    },
+
+    {
+        id: 20,
+        categoria: "Reglamento de Tránsito",
+        question: "Si un vehículo que tiene la intención de sobrepasarlo o adelantarlo lo alcanza, ¿qué debería hacer?",
+        answers: [
+            "Aumentar la velocidad para no dejar que el otro vehículo lo pase.",
+            "No aumentar la velocidad hasta que el vehículo lo sobrepase.",
+            "Disminuir drásticamente la velocidad.",
+            "Detener su vehículo."
+        ],
+        correct: 1,
+        image: null
+    },
+
+    {
+        id: 23,
+        categoria: "SOAT",
+        question: "En caso de accidentes, el SOAT cubre los daños que sufren:",
+        answers: [
+            "Solo los ocupantes del vehículo.",
+            "Los ocupantes y terceros no ocupantes del vehículo.",
+            "Solo terceros afectados.",
+            "Solo el conductor del vehículo."
+        ],
+        correct: 1,
+        image: null
+    },
+
+    {
+        id: 37,
+        categoria: "Preferencia de paso",
+        question: "En una rotonda, tiene prioridad de paso el vehículo que:",
+        answers: [
+            "Quiere ingresar a la rotonda.",
+            "Circula por ella.",
+            "Acelera primero.",
+            "Hace sonar la bocina."
+        ],
+        correct: 1,
+        image: null
+    },
+
+    {
+        id: 39,
+        categoria: "Seguridad vial",
+        question: "Si un conductor está tomando medicamentos y por ello siente sueño, ¿qué debe hacer?",
+        answers: [
+            "Manejar normalmente.",
+            "Manejar despacio.",
+            "Abstenerse de manejar.",
+            "Manejar con un copiloto."
+        ],
+        correct: 2,
+        image: null
+    }
+
+];
